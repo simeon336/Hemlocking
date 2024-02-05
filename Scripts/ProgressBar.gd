e35ba0257fa675a7c649555bff0246ea7b9ec0ce
simeon336 <simeon.m.yordanov.2019@elsys-bg.org>
2024-02-05 16:50:16 +0200

    fixed hp bar

--- Scripts/ProgressBar.gd
+++ Scripts/ProgressBar.gd
@@ -1,16 +1,40 @@
 extends ProgressBar
 
-@onready var player : CharacterBody2D = get_node("res://Scenes/player.tscn")  # Change the path accordingly
+var max_hp : int
+var hp : int
 
 func _ready():
-	pass
+	load_game()
 
 func _on_player_health_changed():
 	update()
 
 func update():
-	value = player.hp * 100 / player.max_hp
+	value = hp * 100 / max_hp
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func load_game():
+	if not FileAccess.file_exists("res://Scenes/savegame.json"):
+		return # Error! We don't have a save to load.
+	var save_game = FileAccess.open("res://Scenes/savegame.json", FileAccess.READ)
+	while save_game.get_position() < save_game.get_length():
+		var json_string = save_game.get_line()
+
+		# Creates the helper class to interact with JSON
+		var json = JSON.new()
+
+		# Check if there is any error while parsing the JSON string, skip in case of failure
+		var parse_result = json.parse(json_string)
+		if not parse_result == OK:
+			print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+			continue
+
+		# Get the data from the JSON object
+		var node_data = json.get_data()
+		
+		max_hp = node_data["max_hp"]
+		hp = node_data["hp"]
+		update()
